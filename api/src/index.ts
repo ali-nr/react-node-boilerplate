66f4a72c7e618882649b0fb58ec32c596c17c509
Alireza Noori <ali@rotageek.com>
2020-07-27 08:47:49 +1000

    updated setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,7 +3,7 @@ import {config} from 'dotenv';
 //initialise config variables
 config();
 
-console.log(process.env.SPOTIFY_CLIENTID);
+console.log(process.env.SOME_VALUE);
 
 const typeDefs = gql`
   type Book {
